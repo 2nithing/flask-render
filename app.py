c52fc3dc7951f51c1e0680f55d6781a58170eb88
Nithin G <79520840+2nithing@users.noreply.github.com>
2023-08-21 12:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template, request
+import weight_predict
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods = ['GET','POST'])
+def hello():
+    if request.method == 'POST':
+        height = request.form['height']
+        weight = weight_predict.prediction([[float(height)]])
+        return render_template('index.html',weight = round(weight[0],2), height=height)
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
